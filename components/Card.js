2a3fab10adec44790ddf8047328d0d2c3cecfb04
Shivam Vijaywargi <vjshivam5@gmail.com>
2022-11-10 15:11:56 +0530

    NEW: Completed cards section - first 3

--- components/Card.js
+++ components/Card.js
@@ -4,11 +4,23 @@ const Card = ({
   card: { heading, content, image, direction, backgroundColor },
 }) => {
   return (
-    <div className={`${backgroundColor ? "bg-[#f6f6f6]" : "bg-white"}`}>
-      <div className={`p-6 my-10 space-y-4`}>
-        <img src={image} alt={heading} />
-        <h2 className="font-[700] text-lg leading-6">{heading}</h2>
-        <p className="text-sm">{content}</p>
+    <div className={`${backgroundColor ? "bg-[#f6f6f6] py-3" : "bg-white"}`}>
+      <div
+        className={`p-6 md:container md:mx-auto lg:p-12 my-10 space-y-4 md:flex md:items-center md:justify-between xl:justify-between lg:gap-20 ${
+          direction ? "md:flex-row" : "md:flex-row-reverse"
+        }`}
+      >
+        <div className="md:flex-1 lg:flex-[1.5]">
+          <img src={image} alt={heading} />
+        </div>
+        <div className="md:flex-1">
+          <h2 className="font-[700] text-gray-800 text-lg leading-6 md:text-5xl md:max-w-xs">
+            {heading}
+          </h2>
+          <p className="text-sm md:text-lg leading-6 text-gray-500 md:mt-8">
+            {content}
+          </p>
+        </div>
       </div>
     </div>
   );
--- data/data.js
+++ data/data.js
@@ -4,8 +4,8 @@ const cardsData = [
     content:
       "Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.",
     image: "/assets/invite-only.svg",
-    direction: "row",
-    backgroundColor: false,
+    direction: true, // true = row
+    backgroundColor: false, // false = white
     id: 1,
   },
   {
@@ -13,8 +13,8 @@ const cardsData = [
     content:
       "Grab a seat in a voice channel when you’re free. Friends in your server can see you’re around and instantly pop in to talk without having to call.",
     image: "/assets/hang-out.svg",
-    direction: "row-reverse",
-    backgroundColor: true,
+    direction: false, // false = row-reverse
+    backgroundColor: true, // true = #f6f6f6
     id: 2,
   },
   {
@@ -22,8 +22,8 @@ const cardsData = [
     content:
       "Get any community running with moderation tools and custom member access. Give members special powers, set up private channels, and more.",
     image: "/assets/few-to-fandom.svg",
-    direction: "row",
-    backgroundColor: false,
+    direction: true, // true = row
+    backgroundColor: false, // false = white
     id: 3,
   },
 ];
